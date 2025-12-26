51c0cdc9877e92ce9b5959f54e93e9385b84dc45
Aishwarya4105 <aishwarya4102025@gmail.com>
2025-12-26 15:02:26 +0530

    Update BookingSucess.js

--- frontend/src/AgencyManagementSys/BookingSucess.js
+++ frontend/src/AgencyManagementSys/BookingSucess.js
@@ -1,21 +1,11 @@
-/*const BookingSucess =()=>{
-    return(
-        <div>
-            <h2>Booking  Sucessful!</h2>
-            <p>Thank you for booking.Your travel package will be confirm by Agency.Stay tune!!</p>
-        </div>
-    )
-};
-export default BookingSucess;*/
 
 
 import React from 'react';
 import './agency.css';
-import { useNavigate } from 'react-router-dom'; // Ensure this is imported
+import { useNavigate } from 'react-router-dom'; 
 
 const BookingSucess = () => {
-    const navigate = useNavigate(); // <--- YOU WERE MISSING THIS LINE
-
+    const navigate = useNavigate();
     return (
         <div className='sucess-container'>
             <h2>Booking Successful!</h2>
